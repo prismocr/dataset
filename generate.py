2d92f0b0886d1f8697c4d8455b92a913556f6ef5
Naexys <naexys.naexys@gmail.com>
2020-11-14 22:07:22 +0100

    CSV dataset

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from PIL import Image, ImageDraw, ImageFont
 from utils import *
 from matplotlib import font_manager
@@ -17,42 +18,37 @@ def mkdir(path):
         print(f"Creation of the directory {path} failed")
 
 
-def generate_directories(path):
-    """ generate directories
-
-    :param path: local path
-    :return:  None
-    """
+def generate_dataset_directories(path):
     path = os.path.join(path, "dataset")
     mkdir(path)
-    for i in range(26):
-        local_path = os.path.join(path, chr(97 + i))
-        mkdir(local_path)
-        uppercase_path = os.path.join(local_path, "uppercase")
-        lowercase_path = os.path.join(local_path, "lowercase")
-        mkdir(uppercase_path)
-        mkdir(lowercase_path)
+    path = os.path.join(path, "images")
+    mkdir(path)
 
 
 def generate_dataset(path: str, fonts, fonts_path, font_size=28):
     n = 0
-    path = os.path.join(path, "dataset")
-    for font, font_path in zip(fonts, fonts_path):
-        n += 1
-        for i in range(26):
-            char = chr(97 + i)
-            local_path = os.path.join(path, char)
-            uppercase_path = os.path.join(local_path, f"uppercase")
-            lowercase_path = os.path.join(local_path, f"lowercase")
-            try:
-                fnt = ImageFont.truetype(font, font_size)
-                write_on_image(fnt, chr(65 + i), uppercase_path, font)
-                write_on_image(fnt, char, lowercase_path , font)
-            except:
-                print(f"Font : {font_path} is not loadable")
-                n -= 1
-                break
-    print(f"saved {n} fonts")
+    index = 0
+    path_to_csv = os.path.join(path, "dataset", "data.csv")
+    path = os.path.join(path, "dataset", "images")
+    with open(path_to_csv, "w", newline='', encoding='utf-8') as f_csv: 
+        f_writer = csv.writer(f_csv, delimiter=',')
+
+        for font, font_path in zip(fonts, fonts_path):
+            n += 1
+            for i in range(26):
+                chars = [chr(97 + i), chr(65 + i)]
+                try:
+                    fnt = ImageFont.truetype(font, font_size)
+                    for char in chars:
+                        filename = f"image_{index}.bmp"
+                        write_on_image(fnt, char, path, filename)
+                        index += 1
+                        f_writer.writerow([char] + [filename] + [font])
+                except:
+                    print(f"Font : {font_path} is not loadable")
+                    n -= 1
+                    break
+        print(f"saved {n} fonts")
         
 
 def get_clear_font_name(fonts):
@@ -62,14 +58,14 @@ def get_clear_font_name(fonts):
     return clear
 
 
-def write_on_image(font, text :str, path_to_save :str, font_name :str, size=(28, 28)):
+def write_on_image(font, text :str, path_to_save :str, name_to_save :str, size=(28, 28)):
     width, height = size
     img = Image.new("L", (width, height), color=0)
     draw = ImageDraw.Draw(img)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     draw.text(((width-w)/2, (height-h)/2), text=text, fill='white', font=font)
-    path_to_save = os.path.join(path_to_save, f"{font_name.lower()}.bmp")
+    path_to_save = os.path.join(path_to_save, name_to_save)
     img.save(path_to_save)
 
 
@@ -84,3 +80,4 @@ def get_all_fonts():
     fonts.sort()
     return fonts
     
+
--- main.py
+++ main.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     fonts = ["calibri.ttf", "LiberationSerif-Regular.ttf", "arial.ttf", "georgia.ttf", "DejaVuSans.ttf"]
 
     fonts_clear = get_clear_font_name(fonts)
-    generate_directories(path)
+    generate_dataset_directories(path)
     generate_dataset(path, fonts_clear, fonts)
