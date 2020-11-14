f5128d0031d06c7e975cc9becfd1803e53d995b7
Naexys <naexys.naexys@gmail.com>
2020-11-14 20:57:58 +0100

    WIP dataset

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from utils import *
+from matplotlib import font_manager
 
 
 def mkdir(path):
@@ -27,28 +28,59 @@ def generate_directories(path):
     for i in range(26):
         local_path = os.path.join(path, chr(97 + i))
         mkdir(local_path)
-        mkdir(os.path.join(local_path, "clear"))
+        uppercase_path = os.path.join(local_path, "uppercase")
+        lowercase_path = os.path.join(local_path, "lowercase")
+        mkdir(uppercase_path)
+        mkdir(lowercase_path)
 
 
-def generate_dataset_clear(path):
-    """ generate a clear dataset if the folders exist
-
-    :param path: local path
-    :return: None
-    """
-    path_letter_init = os.path.join(path, "alphabet")
+def generate_dataset(path: str, fonts, fonts_path, font_size=28):
+    n = 0
     path = os.path.join(path, "dataset")
-    for i in range(26):
-        local_path = os.path.join(path, chr(97 + i))
-        path_letter = os.path.join(path_letter_init, chr(97 + i))
+    for font, font_path in zip(fonts, fonts_path):
+        n += 1
+        for i in range(26):
+            char = chr(97 + i)
+            local_path = os.path.join(path, char)
+            uppercase_path = os.path.join(local_path, f"uppercase")
+            lowercase_path = os.path.join(local_path, f"lowercase")
+            try:
+                fnt = ImageFont.truetype(font, font_size)
+                write_on_image(fnt, chr(65 + i), lowercase_path, font)
+                write_on_image(fnt, char, uppercase_path, font)
+            except:
+                print(f"Font : {font_path} is not loadable")
+                n -= 1
+                break
+    print(f"saved {n} fonts")
+        
 
-        path_load = path_letter + ".png"
-        im_init = Image.open(path_load)
+def get_clear_font_name(fonts):
+    clear = []
+    for font in fonts:
+        clear.append(os.path.split(font.strip(".ttf"))[-1])
+    return clear
 
-        for j in range(0, 10):
-            im_offset = shift(im_init, 2 * (j + 1))
-            path_save = os.path.join(local_path, os.path.join("clear", f"offset_{j}.png"))
-            im_offset.save(path_save)
-            im = rotate(im_offset, 2 * (j + 1))
-            path_save = os.path.join(local_path, os.path.join("clear", f"offset_rotate_{j}.png"))
-            im.save(path_save)
+
+def write_on_image(font, text :str, path_to_save :str, font_name :str, size=(28, 28)):
+    width, height = size
+    img = Image.new("L", (width, height), color=0)
+    draw = ImageDraw.Draw(img)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    draw.text(((width-w)/2, (height-h)/2), text=text, fill='white', font=font)
+    path_to_save = os.path.join(path_to_save, f"{font_name.lower()}.bmp")
+    img.save(path_to_save)
+
+
+def get_all_fonts():
+    fonts = []
+    for x in font_manager.win32InstalledFonts():
+        x = x[::-1]
+        dot = x.find('.')
+        slash = x.find('\\')
+        x = x[slash-1:dot:-1]
+        fonts += [x]
+    fonts.sort()
+    return fonts
+    
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from generate import *
+from matplotlib import font_manager
+
+
 
 if __name__ == "__main__":
     path = os.getcwd()
+    fonts = get_all_fonts()
+
+    fonts_clear = get_clear_font_name(fonts)
     generate_directories(path)
-    generate_dataset_clear(path)
+    generate_dataset(path, fonts_clear, fonts)
